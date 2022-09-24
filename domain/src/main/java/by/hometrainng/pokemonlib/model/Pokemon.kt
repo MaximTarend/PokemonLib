029e1af4a941fe3b0e869fcac4f7afa707aa0ebb
Maxim Tarend <84102129+MaximTarend@users.noreply.github.com>
2022-09-24 18:23:42 +0300

    Pokemon models for domain module

---
+++ domain/src/main/java/by/hometrainng/pokemonlib/model/Pokemon.kt
@@ -0,0 +1,6 @@
+package by.hometrainng.pokemonlib.model
+
+data class Pokemon(
+    val id: Int,
+    val name: String
+)
---
+++ domain/src/main/java/by/hometrainng/pokemonlib/model/PokemonDetails.kt
@@ -0,0 +1,10 @@
+package by.hometrainng.pokemonlib.model
+
+data class PokemonDetails(
+    val id: Int,
+    val name: String,
+    val type: String,
+    val weight: Int,
+    val height: Int,
+    val imageURL: String
+)
