9a1db784eb30c1a5498bccd1eca14dca3abe62d8
Maxim Tarend <84102129+MaximTarend@users.noreply.github.com>
2022-09-24 19:24:56 +0300

    Room database + model for it + Koin module

--- data/src/main/java/by/hometrainng/pokemonlib/database/PokemonDatabase.kt
+++ data/src/main/java/by/hometrainng/pokemonlib/database/PokemonDatabase.kt
@@ -1,10 +1,12 @@
 package by.hometrainng.pokemonlib.database
 
 import androidx.room.Database
+
+import androidx.room.RoomDatabase
 import by.hometrainng.pokemonlib.model.PokemonEntity
 
 @Database(entities = [PokemonEntity::class], version = 1)
-internal abstract class PokemonDatabase {
+internal abstract class PokemonDatabase: RoomDatabase() {
 
     abstract fun pokemonDao(): PokemonDao
 }
---
+++ data/src/main/java/by/hometrainng/pokemonlib/koin/DatabaseModule.kt
@@ -0,0 +1,17 @@
+package by.hometrainng.pokemonlib.koin
+
+import androidx.room.Room
+import by.hometrainng.pokemonlib.database.PokemonDatabase
+import org.koin.dsl.module
+
+internal val DatabaseModule = module {
+    single {
+        Room.databaseBuilder(
+            get(),
+            PokemonDatabase::class.java,
+            "pokemon_database.db"
+        )
+    }
+
+    single { get<PokemonDatabase>().pokemonDao() }
+}
