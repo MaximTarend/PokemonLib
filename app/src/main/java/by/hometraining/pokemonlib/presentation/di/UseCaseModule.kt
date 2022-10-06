e2d6a0d70b942647716b1b3cd6883e263bcd0bcf
Maxim Tarend <84102129+MaximTarend@users.noreply.github.com>
2022-10-06 15:07:25 +0300

    Separating 'saving pokemon list to DB' to own UseCase + adding it to DI

--- app/src/main/java/by/hometraining/pokemonlib/presentation/di/UseCaseModule.kt
+++ app/src/main/java/by/hometraining/pokemonlib/presentation/di/UseCaseModule.kt
@@ -4,8 +4,10 @@ import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 import by.hometraining.pokemonlib.domain.usecase.GetAllPokemonsUseCase
 import by.hometraining.pokemonlib.domain.usecase.GetPokemonDetailsUseCase
+import by.hometraining.pokemonlib.domain.usecase.SaveAllPokemonsUseCase
 
 internal val useCaseModule = module {
     factoryOf(::GetAllPokemonsUseCase)
     factoryOf(::GetPokemonDetailsUseCase)
+    factoryOf(::SaveAllPokemonsUseCase)
 }
